fca4fb988143856e2a2004a80047e63d4599c7ed
Ruben Manoj <ruben.manoj@edstem.com>
2024-11-26 14:33:58 +0530

    Updated Calculator.java
    1. Included a loop for multiple calculations.
    2. Used constants for menu options instead of magic numbers.

--- Calculator.java
+++ Calculator.java
@@ -4,56 +4,70 @@ import java.util.Scanner;
 
 public class Calculator {
     public static double add(double a, double b) {
-        return a+b;
+        return a + b;
     }
+
     public static double subtract(double a, double b) {
-        return a-b;
+        return a - b;
     }
+
     public static double multiply(double a, double b) {
-        return a*b;
+        return a * b;
     }
+
     public static double divide(double a, double b) {
-        return a/b;
+        return a / b;
     }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Calculator ob = new Calculator();
         double res;
-        System.out.print("Enter first number: ");
-        double op1 = scanner.nextDouble();
-        System.out.print("Enter second number: ");
-        double op2 = scanner.nextDouble();
-        System.out.println( "Choose operation: \n" +
-                            "1. Add (+)\n" +
-                            "2. Subtract (-)\n" +
-                            "3. Multiply (*)\n" +
-                            "4. Divide (/)");
-        System.out.print("Enter choice:");
-        int i = scanner.nextInt();
-        switch(i) {
-            case 1:
-                res = ob.add(op1,op2);
-                System.out.println(op1+" + "+op2+" = "+res);
-                break;
-            case 2:
-                res = ob.subtract(op1, op2);
-                System.out.println(op1+" - "+op2+" = "+res);
-                break;
-            case 3:
-                res = ob.multiply(op1, op2);
-                System.out.println(op1+" * "+op2+" = "+res);
-                break;
-            case 4:
-                if (op2 != 0) {
-                    res = ob.divide(op1, op2);
-                    System.out.println(op1+" / "+op2+" = "+res);
+        String choice;
+        do {
+            System.out.print("Enter first number: ");
+            double op1 = scanner.nextDouble();
+            System.out.print("Enter second number: ");
+            double op2 = scanner.nextDouble();
+            System.out.println("""
+                    Choose operation:\s
+                    1. Add (+)
+                    2. Subtract (-)
+                    3. Multiply (*)
+                    4. Divide (/)""");
+            System.out.print("Enter operation you wish to perform: ");
+            String option = scanner.next();
+            switch (option) {
+                case "add":
+                case "ADD":
+                    res = ob.add(op1, op2);
+                    System.out.println(op1 + " + " + op2 + " = " + res);
                     break;
-                } else {
-                    System.out.println("Undefined because denominator is zero");
+                case "subtract":
+                case "SUBTRACT":
+                    res = ob.subtract(op1, op2);
+                    System.out.println(op1 + " - " + op2 + " = " + res);
                     break;
-                }
-            default:
-                break;
-        }
+                case "multiply":
+                case "MULTIPLY":
+                    res = ob.multiply(op1, op2);
+                    System.out.println(op1 + " * " + op2 + " = " + res);
+                    break;
+                case "divide":
+                case "DIVIDE":
+                    if (op2 == 0) {
+                        System.out.println("Undefined because denominator is zero");
+                        break;
+                    } else {
+                        res = ob.divide(op1, op2);
+                        System.out.println(op1 + " / " + op2 + " = " + res);
+                        break;
+                    }
+                default:
+                    break;
+            }
+            System.out.print("Do you wish to perform another calculation? (yes/no): ");
+            choice = scanner.next();
+        }while(choice.equals("yes"));
     }
 }
