c1105375017367d8afb48e5b0d557e8faaf4c58a
Ruben Manoj <ruben.manoj@edstem.com>
2024-11-26 11:46:52 +0530

    QuadraticSoln.java
    A quadratic equation a x² + b x + c = 0 has either 0, 1, or 2 distinct solutions for real values of x. Given a, b and c, you should return the number of solutions to the equation.
    
    Examples
    
    solutions(1, 0, -1) ➞ 2
    // x² - 1 = 0 has two solutions (x = 1 and x = -1).
    
    solutions(1, 0, 0) ➞ 1
    // x² = 0 has one solution (x = 0).
    
    solutions(1, 0, 1) ➞ 0
    // x² + 1 = 0 has no real solutions.
    
    Notes
    
    You do not have to calculate the solutions, just return how many there are.
    a will always be non-zero.

---
+++ Medium/QuadraticSoln.java
@@ -0,0 +1,16 @@
+//How Many Solutions Does This Quadratic Have
+
+public class Challenge {
+	public static int solutions(int a, int b, int c) {
+		int d = (b*b)-(4*a*c);
+		if(d>0) {
+			return 2;
+		}
+		else if(d==0) {
+			return 1;
+		}
+		else {
+			return 0;
+		}
+  }
+}
