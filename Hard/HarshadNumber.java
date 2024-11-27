4f99327054f97ad83158a95ce2e8753e2c8f0533
Ruben Manoj <ruben.manoj@edstem.com>
2024-11-27 09:35:25 +0530

    Updated HarshadNumber.java
    added full code

--- Hard/HarshadNumber.java
+++ Hard/HarshadNumber.java
@@ -1,12 +1,30 @@
 //Recursion: Harshad Number
+package edabit.java.ruben;
 
 public class HarshadNumber {
-	public static boolean isHarshad(int n) {
-		int r=0,i=n;
-		while(i>0) {
-			r+=i%10;
-			i/=10;
-		}
-		return n%r==0;
-	}
+    public static int calcDigitSum(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        return calcDigitSum(n / 10) + n % 10;
+    }
+
+    public static boolean isHarshad(int n) {
+        if (n == 0) {
+            return false;
+        }
+        int digitSum = calcDigitSum(n);
+        return n % digitSum == 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isHarshad(75)); //false
+        System.out.println(isHarshad(171)); //true
+        System.out.println(isHarshad(481)); //true
+        System.out.println(isHarshad(89)); //false
+        System.out.println(isHarshad(516)); //true
+        System.out.println(isHarshad(214)); //false
+        System.out.println(isHarshad(209)); //true
+        System.out.println(isHarshad(12335)); //false
+    }
 }
